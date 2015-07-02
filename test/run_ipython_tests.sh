78c031d8bac0aee0ec360ee818e9461520ae92c6
marscher <m.scherer@fu-berlin.de>
2015-07-02 14:19:30 +0200

    some comments

--- test/run_ipython_tests.sh
+++ test/run_ipython_tests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Run ipython notebook tests
 
-#ipynb_dir=.
 base_dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 ipynb_dir=$base_dir/..
-#cd examples/ipython
 pushd $ipynb_dir
 testfail=0
+##############################################################################
+# add new notebooks here and increment the testfail number to distinct failing
+# notebooks upon errors.
 python $base_dir/ipynbtest.py "its.ipynb" || testfail=1
 
+
+##############################################################################
+# leave this alone
 popd 
 if [ $testfail -gt 0 ]
 then
