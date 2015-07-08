7d122d3486ecf5045e0433c9b6a649b6a8bad81a
Nuria <nuria.plattner@fu-berlin.de>
2015-07-08 17:16:55 +0200

    replaced old TPT notebooks by new versions

--- test/run_ipython_tests.sh
+++ test/run_ipython_tests.sh
@@ -14,9 +14,10 @@ python $base_dir/ipynbtest.py "Estimate_BPTI_MSM.ipynb" || testfail=2
 popd
 pushd TPT
 python $base_dir/ipynbtest.py "TPT_BPTI.ipynb" || testfail=3
+python $base_dir/ipynbtest.py "TPT_AlanineDiP.ipynb" || testfail=4
 popd
 pushd Analyze_MSM
-python $base_dir/ipynbtest.py "Analyze_MSM_AlanineDiP.ipynb" || testfail=4
+python $base_dir/ipynbtest.py "Analyze_MSM_AlanineDiP.ipynb" || testfail=5
 popd
 
 ##############################################################################
