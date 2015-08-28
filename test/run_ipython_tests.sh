0f2660481727a6c693828403e7add4bb8d4f1415
noe <frank.noe@fu-berlin.de>
2015-08-28 03:12:46 +0200

    Updated pentapeptide to 2.0

--- test/run_ipython_tests.sh
+++ test/run_ipython_tests.sh
@@ -8,10 +8,30 @@ testfail=0
 ##############################################################################
 # add new notebooks here and increment the testfail number to distinct failing
 # notebooks upon errors.
-pushd BPTI
+
+###################
+# TEST APPLICATIONS
+
+# BPTI
+pushd applications/bpti_msm
 python $base_dir/ipynbtest.py "Estimate_BPTI_MSM.ipynb" || testfail=1
 python $base_dir/ipynbtest.py "TPT_BPTI.ipynb" || testfail=2
 popd
+
+##############
+# TEST METHODS
+
+# Model selection/validation
+pushd methods/model_selection_validation
+python $base_dir/ipynbtest.py "Model_Estimator_Test.ipynb" || testfail=1
+popd
+
+# TPT
+pushd methods/tpt
+python $base_dir/ipynbtest.py "tpt.ipynb" || testfail=1
+popd
+
+
 #pushd md2msm_penta_peptide
 #python $base_dir/ipynbtest.py "md2msm_penta_peptide.ipynb" || testfail=2
 #popd
